9c026dc39627fda898ff4ae3c2857efa4031f58e
Viktor <vic99999@yandex.ru>
2021-01-01 13:50:38 +0100

    Update gcd.js

--- gcd.js
+++ gcd.js
@@ -32,13 +32,14 @@ function EuclidsGCD(a, b) {
 
 // https://github.com/tc39/proposal-bigint/issues/205
 // https://github.com/tc39/ecma262/issues/1729
+// floor(log2(a)) + 1 if a > 0
 function bitLength(a) {
   const s = a.toString(16);
-  const c = s.charCodeAt(0);
-  if (c === '-'.charCodeAt(0) || c === '0'.charCodeAt(0)) {
+  const c = s.charCodeAt(0) - '0'.charCodeAt(0);
+  if (c <= 0) {
     throw new RangeError();
   }
-  return (s.length - 1) * 4 + (32 - Math.clz32(Math.min(c - '0'.charCodeAt(0), 8)));
+  return (s.length - 1) * 4 + (32 - Math.clz32(Math.min(c, 8)));
 }
 
 // https://en.wikipedia.org/wiki/Lehmer%27s_GCD_algorithm
